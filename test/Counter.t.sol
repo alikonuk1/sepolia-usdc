0218a9d9d516cd4d6565b8c317aad15b502b0081
alikonuk1 <alikonuk1@gmail.com>
2023-08-28 22:54:09 +0300

    init

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,23 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/mockUSDC.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract mockUSDCTest is Test {
+    mockUSDC public usdc;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        usdc = new mockUSDC();
+        usdc.setNumber(0);
     }
 
     function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+        usdc.increment();
+        assertEq(usdc.number(), 1);
     }
 
     function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        usdc.setNumber(x);
+        assertEq(usdc.number(), x);
     }
 }
